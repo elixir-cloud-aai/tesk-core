fbc77c124decda4876f92a61e28be439a8a3dca2
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2018-08-29 13:03:29 +0100

    enhancement: basic tests for job

--- tests/test_job.py
+++ tests/test_job.py
@@ -1,7 +1,33 @@
+import pytest
+
+from kubernetes.client import V1DeleteOptions
 from tesk_core.job import Job
 
 
-def test_job():
-    job = Job({'metadata': {'name': 'test'}})
-    assert job.name == 'task-job'
-    assert job.namespace == 'default'
+@pytest.fixture
+def default_job(mocker):
+    mocker.spy(Job, '_k8s_batch')
+    body = {
+        'metadata': {'name': 'default'},
+    }
+    return Job(body)
+
+def test_attributes(default_job):
+    assert default_job.name == 'task-job'
+    assert default_job.namespace == 'default'
+    assert default_job.k8s_job['metadata']['name'] == 'task-job'
+
+def test_deletion(default_job):
+    # check side-effects of deleting the job
+    default_job.delete()
+    default_job._k8s_batch.delete_namespaced_job.assert_called_once_with(
+        default_job.name,
+        default_job.namespace,
+        V1DeleteOptions()
+    )
+
+def test_run(default_job):
+    def never_cancel():
+        return true
+
+    default_job.run_to_completion(1, never_cancel)
